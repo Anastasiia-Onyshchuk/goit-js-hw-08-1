ba8e0fbb1b0f9097ce74fe9001fda18a0aab743c
Anastasiia-Onyshchuk <nastyaonyshchuk07@gmail.com>
2023-08-13 13:19:40 +0300

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
+const container = document.querySelector('.gallery')
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+    <li data-id ='' class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}"  alt="${description}"/>
+    </a>
+    </li>`
+    ).join('');   
+    console.log(arr);
+}
+createMarkup(galleryItems);
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+console.log(SimpleLightbox);
+// container.addEventListener('click', onImgClick)
+function onClick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+}
+    const lightbox = new SimpleLightbox('.gallery__item a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+    lightbox.show();
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+
+const localStorageKey = 'videoplayer-current-time';
+player.on('timeupdate', throttle(onPlay, 1000))
+function onPlay(event) {
+   const currentTime = event.seconds;
+  localStorage.setItem(localStorageKey, currentTime); 
+}
+
+const savedTime = parseFloat(localStorage.getItem(localStorageKey));
+if (savedTime) {
+    player.setCurrentTime(savedTime)
+}
